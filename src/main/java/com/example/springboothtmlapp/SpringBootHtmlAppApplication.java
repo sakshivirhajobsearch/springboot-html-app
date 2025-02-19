bcbba1dd505029f75379183b6608357d64db7178
Anurag Bharti <anuragvasubharti@gmail.com>
2025-02-19 16:07:55 +0530

    Added code

---
+++ src/main/java/com/example/springboothtmlapp/SpringBootHtmlAppApplication.java
@@ -0,0 +1,11 @@
+package com.example.springboothtmlapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHtmlAppApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHtmlAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/test/SpringBootHtmlAppApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.example.springboothtmlapp.SpringBootHtmlAppApplication;
+
+@SpringBootTest
+@ContextConfiguration(classes = SpringBootHtmlAppApplication.class)
+public class SpringBootHtmlAppApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
